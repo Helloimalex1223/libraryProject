a8a20efb5ffbb5c63debe71f517f0c72a0e6a558
Helloimalex1223 <plattaalex@gmail.com>
2024-07-08 11:25:33 -0900

    Add functions for getting input from a form

--- script.js
+++ script.js
@@ -9,24 +9,40 @@ function Book(title, author, pages, read)
     this.read = read;
 }
 
-function addBookToLibrary()
-{
+letbookVal = 0;
 
+
+//adds book to library based on the user's input
+function addBookToLibrary(title, author, pages, read)
+{
+    let myBook = new Book(title, author, pages, read);
+    myLibrary.push(myBook);
 }
 
 
-let myBook = new Book("JEE", "JERJEK", 2323, true);
-let myBook2 = new Book("JEE", "JERJdsadEK", 2323, true);
-let myBook3 = new Book("JEdsadsE", "JERJdsadEK", 2323, true);
+//get user input from form. 
+
+const submitButton = document.querySelector("#submitButton");
+submitButton.addEventListener("click", function()
+{
+    const formBookTitle = document.getElementById("title").value;
+    const formBookAuthor = document.getElementById("author").value;
+    const formBookPages = document.getElementById("pages").value;
+    const formBookRead = document.getElementById("read").value;
+    addBookToLibrary(formBookTitle, formBookAuthor, formBookPages, formBookRead);  
+})
 
 
-myLibrary.push(myBook);
-myLibrary.push(myBook2);
-myLibrary.push(myBook3);
+// let myBook = new Book("JEE", "JERJEK", 2323, true);
+
+
+// myLibrary.push(myBook);
+// myLibrary.push(myBook2);
+// myLibrary.push(myBook3);
 console.log(myLibrary);
 
 
-
+//add info from myLibrary array to the DOM
 for(let i = 0; i < myLibrary.length; i++)
 {
     const ele = document.getElementById("books");
@@ -44,14 +60,23 @@ for(let i = 0; i < myLibrary.length; i++)
     const pagesPara = document.createElement("p");
     pagesPara.textContent = `Pages: ${myLibrary[i].title}`;
     bookDiv.appendChild(pagesPara);
+
+    const isReadPara = document.createElement("p");
+    isReadPara.textContent = `Read: ${myLibrary[i].read}`;
+    bookDiv.appendChild(isReadPara);
     
     ele.appendChild(bookDiv);
     bookDiv.appendChild(titlePara);
 }
 
-
-//todo: set up the DOM nodes in JavaScript to append a book div to the container
-//get user input
 //user input creates a Book object using the book constructor
 //the book constructor is pushed to the myLibrary function
 //the DOM reads data from the myLibrary array
+
+
+
+
+//todo:
+//finish form functionality (have it update the DOM correctly)
+//Cleanup DOM appends when creating a book object?
+//Create delete button that deletes the array values for a book
