10a1a3de0c7e9d9b94f7c091978e196846e9a28c
Helloimalex1223 <plattaalex@gmail.com>
2024-07-08 12:55:11 -0900

    Add logic to take form submission information and populate array with it

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function addBookToLibrary(title, author, pages, read)
 {
     let myBook = new Book(title, author, pages, read);
     myLibrary.push(myBook);
+    addTolibraryGUI(myBook);
 }
 
-
-//get user input from form. 
+//get user input from form when the Submit button is clicked
 
 const submitButton = document.querySelector("#submitButton");
 submitButton.addEventListener("click", function()
@@ -29,41 +29,54 @@ submitButton.addEventListener("click", function()
     const formBookAuthor = document.getElementById("author").value;
     const formBookPages = document.getElementById("pages").value;
     const formBookRead = document.getElementById("read").value;
-    addBookToLibrary(formBookTitle, formBookAuthor, formBookPages, formBookRead);  
+    addBookToLibrary(formBookTitle, formBookAuthor, formBookPages, formBookRead);
+
+    document.getElementById("addBookForm").reset();    
 })
 
-
-// let myBook = new Book("JEE", "JERJEK", 2323, true);
-
-
-// myLibrary.push(myBook);
-// myLibrary.push(myBook2);
-// myLibrary.push(myBook3);
-console.log(myLibrary);
+//prevent the form from submitting when the Submit button is clicked
+document.getElementById("addBookForm").addEventListener('submit', function(event)
+{event.preventDefault();}
+);
 
 
-//add info from myLibrary array to the DOM
-for(let i = 0; i < myLibrary.length; i++)
+//add the latest book from the myLibrary array to the DOM
+
+function addTolibraryGUI(obj)
 {
     const ele = document.getElementById("books");
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("bookEntry");
     
     const titlePara = document.createElement("p");
-    titlePara.textContent = `Title: ${myLibrary[i].title}`;
+    titlePara.textContent = `Title: ${obj.title}`;
     bookDiv.appendChild(titlePara);
     
     const authorPara = document.createElement("p");
-    authorPara.textContent = `Author: ${myLibrary[i].author}`;
+    authorPara.textContent = `Author: ${obj.author}`;
     bookDiv.appendChild(authorPara);
     
     const pagesPara = document.createElement("p");
-    pagesPara.textContent = `Pages: ${myLibrary[i].title}`;
+    pagesPara.textContent = `Pages: ${obj.title}`;
     bookDiv.appendChild(pagesPara);
 
     const isReadPara = document.createElement("p");
-    isReadPara.textContent = `Read: ${myLibrary[i].read}`;
+    isReadPara.textContent = `Read: ${obj.read}`;
     bookDiv.appendChild(isReadPara);
+
+
+    //add the remove button for each book entry
+    const removeButton = document.createElement("button");
+    removeButton.textContent = `Remove Book`;
+    removeButton.classList.add("remove");
+    bookDiv.appendChild(removeButton);
+
+    //add the delete button for each book entry
+    const readButton = document.createElement("button");
+    readButton.textContent = `Read`;
+    readButton.classList.add("read");
+    bookDiv.appendChild(readButton);
+    
     
     ele.appendChild(bookDiv);
     bookDiv.appendChild(titlePara);
@@ -73,10 +86,12 @@ for(let i = 0; i < myLibrary.length; i++)
 //the book constructor is pushed to the myLibrary function
 //the DOM reads data from the myLibrary array
 
-
-
-
+read.addEventListener("click", function()
+{
+    console.log("CLICKED!");
+})
 //todo:
-//finish form functionality (have it update the DOM correctly)
-//Cleanup DOM appends when creating a book object?
+//Cleanup DOM appends when creating a book object/put the DOM append functionality in the AddBookToLibrary function
 //Create delete button that deletes the array values for a book
+//Create read button that changes the value of the read field
+//Figure out why the DOM is in a weird order
