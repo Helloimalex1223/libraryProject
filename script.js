172f68296d9ebf5e72fc039d8dd6a4954e2a45af
Helloimalex1223 <plattaalex@gmail.com>
2024-07-14 13:53:51 -0900

    Add logic to set data-attribute for each remove button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const myLibrary = [];
 
 function Book(title, author, pages, read)
@@ -37,9 +39,9 @@ submitButton.addEventListener("click", function()
 
 //prevent the form from submitting when the Submit button is clicked
 document.getElementById("addBookForm").addEventListener('submit', function(event)
-{
-    event.preventDefault();
-}
+    {
+        event.preventDefault();
+    }
 );
 
 console.log(myLibrary);
@@ -83,41 +85,57 @@ function addTolibraryGUI(obj)
     readButton.classList.add("read");
     bookDiv.appendChild(readButton);
 
-    //set data-index attribute for each book
-    setBookAttribute();
+    //populates nodelist with latest remove button
+    addListener();
 }
 
 
-
-//add attirbute to each book that contains the bookList class
 let books = document.getElementsByClassName("bookEntry");
-
-function setBookAttribute()
-{
-   for(let i = 0; i < books.length; i++)
-    {
-        books[i].setAttribute('data-index', i);
-    } 
-}
-
-// var delButton = document.querySelector(".delete")
-// delButton.addEventListener("click", function()
-// {
-//     for(let i = 0; i < books.length; i++)
-//     {
-//         console.log(books[i].getAttribute('data-index'));
-//     }
-// })
-
-
-
 
 //user input creates a Book object using the book constructor
 //the book constructor is pushed to the myLibrary function
 //the DOM reads data from the myLibrary array
 
+//adds the event listener to the buttons created in the DOM
+let buttonList;
 
+function addListener()
+{
+    buttonList = document.getElementsByClassName("remove");
 
-//current state: data-attribute list is populating correctly. Need to figoure out how to use the number in the data-attribute to remove the specific DOM node and entry in the array:
-    //for example, button in the book with a data-attribute of 3 is clicked, delete the 3rd entry in the array and remove the 3rd node in the nodelist
-//
+    for(let k = 0; k < buttonList.length; k++)
+    {
+        //sets data-attribute for the list of remove buttons 
+        buttonList[k].addEventListener("click", btnClicked)
+        buttonList[k].setAttribute('data-index', k);
+    }
+}
+
+//when the remove button is clicked, remove the node and book object corresponding to the buttons data-attribute number
+function btnClicked(e)
+{
+    let btn = e.currentTarget;
+    let buttonValue = btn.getAttribute('data-index');
+
+    removeNode(buttonValue);
+}
+
+//adds event listener to the "remove" buttons. When a button is clicked, it triggers the function to remove the node using the button's place in the button list collection
+
+//removes the node from the DOM tree when the "remove" button is clicked
+function removeNode(num)
+{
+    myLibrary.splice(num);
+    books[num].remove();
+    console.log(`my library array:`);
+    console.log(myLibrary);
+
+    console.log(`books array:`);
+    console.log(books);
+    
+    console.log(`Remove button list:`);
+    console.log(buttonList);
+}
+
+//TODO: Bug when I remove a value in the myLibray array -- something wrong with the logic.
+    //Need to test this more
